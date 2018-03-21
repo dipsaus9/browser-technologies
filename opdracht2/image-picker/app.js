b91cb3c1ba041ab9324d191b82da281931e165a0
Dennis <dennis.spierenburg@hotmail.com>
2018-03-21 14:22:18 +0100

    test

--- opdracht2/image-picker/app.js
+++ opdracht2/image-picker/app.js
@@ -1,18 +1,49 @@
 var classListSupport = 'classList' in document.createElement('_');
 var querySelectorSupport = 'querySelector' in document.createElement('_');
+var eventListenerSupport = 'addEventListener' in document.createElement('_');
 var support = false;
 checkSupport();
 start();
 function checkSupport(){
   if(classListSupport){
     if(querySelectorSupport){
-      support = true;
+      if(eventListenerSupport){
+        if(window.XMLHttpRequest){
+          support = true;
+        }
+      }
     }
   }
 }
 
 function start(){
   if(support){
-    console.log('test');
+    var uploadFile = document.querySelector('input[type="file"]');
+    uploadFile.classList.add('enhanched');
+    uploadFile.addEventListener('change', function(e){
+      if(e.target.value !== undefined){
+        var fileName = e.target.value.split( '\\' ).pop();
+        console.log(fileName);
+        var label = document.querySelector('label[for="upload"]');
+        label.innerHTML = fileName;
+        var urlTo = e.target.value;
+        get(urlTo);
+      }
+    })
   }
+}
+function get(url) {
+  return new Promise(function(resolve, reject) {
+    var req = new XMLHttpRequest();
+    req.open('GET', url);
+    req.onload = function() {
+      if (req.status == 200) {
+        resolve(req.response);
+      }
+      else {
+        reject(Error(req.statusText));
+      }
+    };
+    req.send();
+  });
 }
