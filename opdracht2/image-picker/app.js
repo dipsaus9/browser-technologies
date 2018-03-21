8bbe1807e0b19472e26353c10d819383aa184a85
Dennis <dennis.spierenburg@hotmail.com>
2018-03-21 15:01:15 +0100

    updated with js

--- opdracht2/image-picker/app.js
+++ opdracht2/image-picker/app.js
@@ -2,6 +2,7 @@ var classListSupport = 'classList' in document.createElement('_');
 var querySelectorSupport = 'querySelector' in document.createElement('_');
 var eventListenerSupport = 'addEventListener' in document.createElement('_');
 var support = false;
+var number = 4
 checkSupport();
 start();
 function checkSupport(){
@@ -9,7 +10,10 @@ function checkSupport(){
     if(querySelectorSupport){
       if(eventListenerSupport){
         if(window.XMLHttpRequest){
-          support = true;
+          if(window.FileReader){
+            support = true;
+
+          }
         }
       }
     }
@@ -23,28 +27,46 @@ function start(){
     uploadFile.addEventListener('change', function(e){
       if(e.target.value !== undefined){
         var fileName = e.target.value.split( '\\' ).pop();
-        console.log(fileName);
         var label = document.querySelector('label[for="upload"]');
         label.innerHTML = fileName;
-        var urlTo = e.target.value;
-        get(urlTo);
+        var urlTo = e;
+        var reader = new FileReader();
+        number++;
+        reader.onload = function(el){
+          var elementCreated = createImageUpload(reader.result);
+          elementAfter.appendChild(elementCreated);
+          // var test = document.querySelector('input#' + number);
+          // test.checked = true;
+        }
+        reader.readAsDataURL(event.target.files[0]);
+        var elementAfter = document.querySelector('.image-picker_select');
       }
     })
   }
 }
-function get(url) {
-  return new Promise(function(resolve, reject) {
-    var req = new XMLHttpRequest();
-    req.open('GET', url);
-    req.onload = function() {
-      if (req.status == 200) {
-        console.log(req.response);
-        resolve(req.response);
-      }
-      else {
-        reject(Error(req.statusText));
-      }
-    };
-    req.send();
-  });
+
+function createImageUpload(src){
+  console.log(src);
+  var element = document.createElement('label');
+  var input = document.createElement('input');
+  element.setAttribute('for', number);
+  input.setAttribute('id', number);
+  input.setAttribute('type', 'radio');
+  input.setAttribute('value', number);
+  var div1 = document.createElement('div');
+  div1.setAttribute('class', 'containing');
+  var div2 = document.createElement('div');
+  div2.setAttribute('class', 'test');
+  var img = document.createElement('img');
+  img.setAttribute('src', src);
+  var text = document.createElement('p');
+  var textnode = document.createTextNode('Titel');
+
+  div2.appendChild(img);
+  div1.appendChild(div2);
+  text.appendChild(textnode);
+  div1.appendChild(text);
+  element.appendChild(input);
+  element.appendChild(div1);
+  return element;
 }
